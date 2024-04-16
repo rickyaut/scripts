13cf8fdfc469d2bd53f7ef8e03209b0f8fbb3c54
Ricky Liu <ricky.liu@merkleinc.com.au>
2024-04-16 16:26:05 +1000

    add script

---
+++ download-mp3.sh
@@ -0,0 +1,14 @@
+str="${*:1}" 
+IFS=' '     # space is set as delimiter
+read -ra youid <<< "$str"   # str is read into an array as tokens separated by IFS
+
+for i in "${youid[@]}"; do   # access each element of array
+  #Download best video (that also has audio) that is closest in size to 50 MB  
+  #yt-dlp -f "b" -S "filesize~50M" $i
+  # yt-dlp -f "b" -S "height:700" $1
+  yt-dlp -f "ba" -x --audio-format mp3 $i
+done
+mv *.mp3 mp3
+mv *.mp4 mp4
+
+say Download finished
